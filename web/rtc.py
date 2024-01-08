5b74bca45650a359a3858d5bde8c8dfa2bbe02a8
Daniela Berkman <danulea2001@mail.ru>
2024-01-08 19:16:45 +0300

    without bags

--- web/rtc.py
+++ web/rtc.py
@@ -2,6 +2,23 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 from FunkPack.function import Parabola, LinearFunk
+# lin = LinearFunk()
+# par = Parabola()
+
+# Инициализация st.session_state
+if 'linear_params' not in st.session_state:
+    st.session_state.linear_params = {'a': 1.0, 'b': 0.0}
+
+if 'parabola_params' not in st.session_state:
+    st.session_state.parabola_params = {'a': 1.0, 'b': 0.0, 'c': 0.0}
+
+# Инициализация функций
+if 'lin' not in st.session_state:
+    st.session_state.lin = LinearFunk()
+
+if 'par' not in st.session_state:
+    st.session_state.par = Parabola()
+
 
 # Функция для отрисовки графика линейной функции
 def plot_linear_function(ax, lin):
@@ -30,10 +47,14 @@ def main():
     # Определение колонок для размещения элементов
     params_column, plot_column, buttons_column= st.columns(3)
 
-    lin = LinearFunk()
-    par = Parabola()
+    # lin = LinearFunk()
+    # par = Parabola()
 
-    st.session_state.active_button = st.session_state.get("active_button", None)
+    # st.session_state.active_button = st.session_state.get("active_button", None)
+
+    # Определение флага активной кнопки
+    if 'active_button' not in st.session_state:
+        st.session_state.active_button = None
 
     with buttons_column:
         linear_button = st.button("Линейная функция", key="linear_button")
@@ -49,39 +70,42 @@ def main():
 
     if default_button:
         st.session_state.active_button = None
-        linear_a, linear_b = lin.default()
+        linear_a, linear_b = st.session_state.lin.default()
+        parabola_a, parabola_b, parabola_c = st.session_state.par.default()
+
 
     with params_column:
         if st.session_state.active_button == "linear":
-            if 'linear_params' not in st.session_state:
-                st.session_state.linear_params = {'a': 1.0, 'b': 0.0}
+            # if 'linear_params' not in st.session_state:
+            #     st.session_state.linear_params = {'a': 1.0, 'b': 0.0}
                 
             linear_a = st.number_input("Введите значение a для линейной функции", value=st.session_state.linear_params['a'])
             linear_b = st.number_input("Введите значение b для линейной функции", value=st.session_state.linear_params['b'])
-            lin.params = [linear_a, linear_b] 
+            st.session_state.lin.params = [linear_a, linear_b] 
             st.session_state.linear_params = {'a': linear_a, 'b': linear_b}
             
         if st.session_state.active_button == "parabola":
-            if 'parabola_params' not in st.session_state:
-                st.session_state.parabola_params = {'a': 1.0, 'b': 0.0, 'c': 0.0}
+            # if 'parabola_params' not in st.session_state:
+            #     st.session_state.parabola_params = {'a': 1.0, 'b': 0.0, 'c': 0.0}
             
             parabola_a = st.number_input("Введите значение a для параболы", value=st.session_state.parabola_params['a'])
             parabola_b = st.number_input("Введите значение b для параболы", value=st.session_state.parabola_params['b'])
             parabola_c = st.number_input("Введите значение c для параболы", value=st.session_state.parabola_params['c'])
-            par.params = [parabola_a, parabola_b, parabola_c] 
+            st.session_state.par.params = [parabola_a, parabola_b, parabola_c] 
             st.session_state.parabola_params = {'a': parabola_a, 'b': parabola_b, 'c': parabola_c}
+
 
     with plot_column:
         st.subheader("Линейная функция")
         fig1, ax1 = plt.subplots(figsize=(5, 3))
-        plot_linear_function(ax1, lin)
+        plot_linear_function(ax1, st.session_state.lin)
         plt.legend()
         st.pyplot(fig1)
         st.write()
         
         st.subheader("Парабола")
         fig2, ax2 = plt.subplots(figsize=(5, 3))
-        plot_parabola(ax2, par)
+        plot_parabola(ax2, st.session_state.par)
         plt.legend()
         st.pyplot(fig2)
         st.write()
